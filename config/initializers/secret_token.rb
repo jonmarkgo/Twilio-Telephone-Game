0a7cf20fc1ec3bd1ed131c99fa52f785fd76187a
Jonathan Gottfried <jonmarkgo@gmail.com>
2012-05-11 16:35:02 -0400

    New Rails 3.2.3 project with rvmrc and gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwilioTelephoneGame::Application.config.secret_token = '282854d26c76d564d3a2532728bd13ab8789d6a513b007e863ef2030ab63bfa50d82271df74cfb020f5cdbd51b62c5cdaabdcddcb9f850b549b8c91641dbd528'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TwilioTelephoneGame::Application.config.session_store :cookie_store, key: '_Twilio Telephone Game_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TwilioTelephoneGame::Application.config.session_store :active_record_store
