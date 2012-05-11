d79955ff6456de74c71cdd698d726b6dd6791348
Jonathan Gottfried <jonmarkgo@gmail.com>
2012-05-11 16:41:43 -0400

    Added player->game association and acts_as_list for player

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
   attr_accessible :finished, :first_recording, :first_transcription, :last_recording, :last_transcription, :similarity, :started
+  has_many :players, :order => "position"
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,5 @@
 class Player < ActiveRecord::Base
   belongs_to :game
+  acts_as_list :scope => :game
   attr_accessible :name, :phone_number, :position, :recording_url
 end
